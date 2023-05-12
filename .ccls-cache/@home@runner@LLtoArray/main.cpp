22566b2474e620f6d9f852dea5794d87827ec34c
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-12 18:10:00 +0000

    node creation

--- .ccls-cache/@home@runner@LLtoArray/main.cpp
+++ .ccls-cache/@home@runner@LLtoArray/main.cpp
@@ -1,5 +1,39 @@
-#include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-  std::cout << "Hello World!\n";
+class Node {
+  public:
+    std::string val;
+    Node* next;
+
+    Node(std::string initialVal) {
+      val = initialVal;
+      next = nullptr;
+    }
+};
+
+
+
+std::vector<std::string> linkedListValues(Node* head) 
+{
+  std::vector<std::string> temp;
+  
+  Node* current = head;
+  while (current!=nullptr)
+  {
+    temp.push_back(current->val);
+    current=current->next;
+  }
+  return temp;
+}
+
+int main ():
+{
+  Node a("A");
+  Node b("B");
+  Node c("C");
+  Node d("D");
+  a->next=&b;
+  b->next=&c;
+  c->next=&d;
 }
